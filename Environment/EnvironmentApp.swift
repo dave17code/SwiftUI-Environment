3aeef0741c4841920506d57319a7cbe41a45fdda
dave17code <dave17code@gmail.com>
2024-08-01 16:31:17 +0900

    Initial Commit

---
+++ Environment/EnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentApp.swift
+//  Environment
+//
+//  Created by 김두운 on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
